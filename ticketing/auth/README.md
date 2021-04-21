b4bfcb347adc897ae48ae4947c9fccd75d3cc3f5
zoti <tsot85@gmail.com>
2021-04-21 22:09:02 +0300

    error handling complete

--- ticketing/auth/README.md
+++ ticketing/auth/README.md
@@ -1,3 +1,7 @@
 # Generating a tsconfig
 
 `npx tsc --init`
+
+# Bypass chrome "Privacy error"
+
+- click somewhere on the page and type `thisisunsafe`
--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -2,8 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-router.post("api/users/signin", (req, res) => {
-  res.send("Hi there");
+router.post("/api/users/signin", (req, res) => {
+  res.send("Hi there!");
 });
 
 export { router as signinRouter };
