dd2317771d226644264337ba8b185bd4d49f77e4
zoti <tsot85@gmail.com>
2021-05-04 00:42:02 +0300

    working with common

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -2,12 +2,10 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 
-import { validateRequest } from "../middlewares/validate-request";
+import { validateRequest, BadRequestError } from "@zotijstickets/common";
 
 import { Password } from "../services/password";
 import { User } from "../models/user";
-
-import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
