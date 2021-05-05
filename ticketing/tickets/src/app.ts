6cfbb4304626a97eea3522f849415cb14258edca
zoti <tsot85@gmail.com>
2021-05-05 16:34:49 +0300

    crud in progress, common changes

---
+++ ticketing/tickets/src/app.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from "@zotijstickets/common";
+
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+
+const app = express();
+
+app.set("trust proxy", true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    // make 'secure: false' on testing
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
