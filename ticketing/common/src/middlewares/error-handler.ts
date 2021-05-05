d54b64186c7302a2840239e77d4a551f35c9f59e
zoti <tsot85@gmail.com>
2021-05-05 16:48:57 +0300

    problems publishing common project

--- ticketing/common/src/middlewares/error-handler.ts
+++ ticketing/common/src/middlewares/error-handler.ts
@@ -11,6 +11,7 @@ export const errorHandler = (
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
+  // Print more details about the "unknown" error
   console.error(err);
 
   res.status(400).send({
