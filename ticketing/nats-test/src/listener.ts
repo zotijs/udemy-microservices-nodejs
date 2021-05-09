6f31c66b6934714cbeb0e49e8525af5c10a0a805
zoti <tsot85@gmail.com>
2021-05-09 21:10:33 +0300

    added events reusable code to common project

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -1,5 +1,7 @@
-import nats, { Message } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
+
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 console.clear();
 
@@ -14,30 +16,8 @@ stan.on("connect", () => {
     console.log("NATS connection closed");
     process.exit();
   });
-  // setting options for subscription
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true) // manual acknowledgment
-    .setDeliverAllAvailable() // make sure that when the subscription is created NATS delivers all the past events (just the very first time)
-    .setDurableName("orders-service"); // makes sure that failed events will be redelivered
 
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "orders-service-queue-group", // queue group tells NATS not to dump the durable events
-    options
-  );
-
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === "string") {
-      console.log(
-        `Received event number: ${msg.getSequence()}, with data: ${data}`
-      );
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 // close NATS client on SIGINT, SIGTERM
--- ticketing/nats-test/src/publisher.ts
+++ ticketing/nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 console.clear();
 
@@ -6,17 +7,30 @@ const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
-  const data = JSON.stringify({
-    id: "123",
-    title: "concert",
-    price: 20,
-  });
+  const publisher = new TicketCreatedPublisher(stan);
 
-  // all data before send to NATS streaming server need to be stringified
-  stan.publish("ticket:created", data, () => {
-    console.log("Event published!");
-  });
+  try {
+    await publisher.publish({
+      id: "1234",
+      title: "New concert",
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
+  // Before Refactor
+  //   const data = JSON.stringify({
+  //     id: "123",
+  //     title: "concert",
+  //     price: 20,
+  //   });
+
+  //   // all data before send to NATS streaming server need to be stringified
+  //   stan.publish("ticket:created", data, () => {
+  //     console.log("Event published!");
+  //   });
 });
