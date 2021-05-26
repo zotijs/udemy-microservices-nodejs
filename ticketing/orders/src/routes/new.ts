dc767a66f6f325b760985ca5fa4a4687f5628d2e
zoti <tsot85@gmail.com>
2021-05-27 00:16:46 +0300

    orders in progress

---
+++ ticketing/orders/src/routes/new.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import express, { Request, Response } from "express";
+import { requireAuth, validateRequest } from "@zotijstickets/common";
+import { body } from "express-validator";
+
+const router = express.Router();
+
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      // method below couples ticket service, just an example on how to validate input as mongo id
+      // .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
+
+export { router as newOrderRouter };
